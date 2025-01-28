e45bdea1e3ecfe91d324614d34edb5ffa7646a9e
Orrin Hastings <orrin@orrinhastings.com>
2025-01-28 17:50:07 +0100

    adding option to resolve promise early

--- package/android/src/main/java/com/mrousavy/camera/core/types/TakePhotoOptions.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/types/TakePhotoOptions.kt
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.ReadableMap
 import com.mrousavy.camera.core.utils.FileUtils
 import com.mrousavy.camera.core.utils.OutputFile
 
-data class TakePhotoOptions(val file: OutputFile, val flash: Flash, val enableShutterSound: Boolean, val useFastMode: Boolean) {
+data class TakePhotoOptions(val file: OutputFile, val flash: Flash, val enableShutterSound: Boolean, val useFastMode: Boolean, val resolveOnCaptureStarted: Boolean) {
 
   companion object {
     fun fromJS(context: Context, map: ReadableMap): TakePhotoOptions {
@@ -13,9 +13,10 @@ data class TakePhotoOptions(val file: OutputFile, val flash: Flash, val enableSh
       val enableShutterSound = if (map.hasKey("enableShutterSound")) map.getBoolean("enableShutterSound") else false
       val directory = if (map.hasKey("path")) FileUtils.getDirectory(map.getString("path")) else context.cacheDir
       val useFastMode = if (map.hasKey("useFastMode")) map.getBoolean("useFastMode") else false
+      val resolveOnCaptureStarted = if (map.hasKey("resolveOnCaptureStarted")) map.getBoolean("resolveOnCaptureStarted") else false
 
       val outputFile = OutputFile(context, directory, ".jpg")
-      return TakePhotoOptions(outputFile, flash, enableShutterSound, useFastMode)
+      return TakePhotoOptions(outputFile, flash, enableShutterSound, useFastMode, resolveOnCaptureStarted)
     }
   }
 }
