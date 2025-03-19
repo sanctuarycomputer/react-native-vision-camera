82cc52c12ac09988e70aafafbc21d58d238853b4
Orrin Hastings <orrin@orrinhastings.com>
2025-03-19 16:21:07 +0100

    returning photo mediastore Uri

--- package/android/src/main/java/com/mrousavy/camera/core/Photo.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/Photo.kt
@@ -2,4 +2,4 @@ package com.mrousavy.camera.core
 
 import com.mrousavy.camera.core.types.Orientation
 
-data class Photo(val path: String, val width: Int, val height: Int, val orientation: Orientation, val isMirrored: Boolean)
+data class Photo(var path: String, val width: Int, val height: Int, val orientation: Orientation, val isMirrored: Boolean)
