2175c2c83f0dc5a35d8cc57123ce760d783ec340
Orrin Hastings <orrin@orrinhastings.com>
2025-04-09 13:49:48 +0200

    add try/catch incase settings perms aren't available

--- package/android/src/main/java/com/mrousavy/camera/core/CameraSession+Video.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/CameraSession+Video.kt
@@ -137,7 +137,12 @@ fun CameraSession.restoreEndOfStream() {
 }
 
 fun CameraSession.setEISMode(newMode: String) {
-  Log.i(CameraSession.TAG, "EIS: changed from: " + SystemProperties.get("persist.vendor.camera.enableEIS"));
-  SystemProperties.set("persist.vendor.camera.enableEIS", newMode);
-  Log.i(CameraSession.TAG, "EIS: changed to: " + SystemProperties.get("persist.vendor.camera.enableEIS"));
+  try {
+    Log.i(CameraSession.TAG, "EIS: changed from: " + SystemProperties.get("persist.vendor.camera.enableEIS"));
+    SystemProperties.set("persist.vendor.camera.enableEIS", newMode);
+    Log.i(CameraSession.TAG, "EIS: changed to: " + SystemProperties.get("persist.vendor.camera.enableEIS"));
+  }
+  catch (e: Exception) {
+    Log.e(CameraSession.TAG, "Unable to set EIS mode ${e.message}")
+  }
 }
